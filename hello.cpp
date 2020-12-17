0fd7c4ede17853cfb270d743a96b0230f3850a18
soaibsafi <soaib.safi@gmail.com>
2020-12-18 00:19:37 +0100

    :tada:Hello

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<climits>
+
+int main(){
+    short a;
+    int b;
+    long c;
+    long long d;
+    
+    std::cout<< SHRT_MAX <<std::endl;
+    std::cout<< USHRT_MAX <<std::endl;
+    std::cout<< LLONG_MAX <<std::endl;
+    std::cout<< sizeof(d) <<std::endl;
+    return 0;
+}
