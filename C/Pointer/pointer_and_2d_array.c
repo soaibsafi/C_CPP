a54bac11ec2776ce7b4cccad5ba3e2a183541c48
soaibsafi <soaib.safi@gmail.com>
2021-01-29 19:03:04 +0100

    Pointers and multidimentional array

--- C/Pointer/pointer_and_2d_array.c
+++ C/Pointer/pointer_and_2d_array.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+void func(int A[][3])
+{
+}
+
 int main()
 {
     // int B[2][3] = {{2, 4, 6}, {9, 5, 6}};
@@ -19,5 +23,15 @@ int main()
         {{0, 8}, {11, 14}}};
 
     // C = int (*)[2][2]
-    printf("%d %d %d %d\n", C, *C, C[0], &C[0][0]);
+    //printf("%d %d %d %d\n", C, *C, C[0], &C[0][0]);
+
+    //func(C); //cannot convert ‘int (*)[2][2]’ to ‘int*
+
+    int A[2] = {3, 4};
+    //func(A);
+
+    int B[2][3] = {{2, 5}, // B returns int (*)[3]
+                   {7, 9}};
+
+    func(B);
 }
